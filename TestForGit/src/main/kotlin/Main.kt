b6958adbac38447a9aacbc5b9d784e4d0120453e
Galon <stephan.galon@cgm.com>
2023-02-02 16:10:59 +0100

    improvement code in TestForGit

--- TestForGit/src/main/kotlin/Main.kt
+++ TestForGit/src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 import java.lang.Math.abs
+import java.lang.Math.max
+import kotlin.math.min
 
 
 fun main() {
@@ -36,10 +38,9 @@ fun main() {
 //    println(nonNegativeValueSameLastDigit(123145, 12312)); println(nonNegativeValueSameLastDigit(1254, 1255))
 //    println(nonNegativeValueSameLastDigit(12, 12312)); println(nonNegativeValueSameLastDigit(1000, 10))//Question 13
 
-//    val x = intArrayOf(0,1,2,3,4,5)
-//    println(numbersequenceInArrayFinder(x))//Question 14
+//    println(numbersequenceInArrayFinder(intArrayOf(0,1,2,3,4,5)))//Question 14
 
-//    countNumber5NextTo()//Question 15
+    countNumber5NextTo()//Question 15
 
 //    val a = intArrayOf(1,2,3,4,5,3,7,5,3,4,9)
 //    val b = intArrayOf(3,4,5,6,7,22,22,22)
@@ -128,6 +129,7 @@ fun main() {
 
 
 }
+//
 fun sum(x: Int, y: Int) :Int {
     val sum = x + y
     return if(x == y) sum * 3 else sum
@@ -140,88 +142,107 @@ fun trueOrNot(x: Int, y: Int) :Boolean{
     return if (x + y == 30 || x == 30 || y == 30) true else false
 }
 fun whithin(x: Int) :Boolean{
-    return if (x< 200 && x> 10) true else false
+//    return if (x> 10 && x< 200) true else false
+    return if (x in 11 until 200) true else false
 }
 fun multiple(x: Int) :Boolean{
-    return if(x % 7 == 0) true else if(x % 3 == 0) true else false
+//    return if(x % 7 == 0) true else if(x % 3 == 0) true else false
+
+    return x%7==0 || x%3==0
 }
 fun temperatures(x: Int, y: Int) :Boolean{
-    return if ((x < 0 && y > 100)||(y < 0 && x > 100)) true else false
+//    return if ((x < 0 && y > 100)||(y < 0 && x > 100)) true else false
+    val min = min(x , y)
+    val max = max(x , y)
+    return min < 0 && max > 100
+
 }
 fun between(x: Int, y: Int) :Boolean{
-    return if((x >= 100 && x <= 200)||(y >= 100 && y <= 200)) true else false
+//    return if((x >= 100 && x <= 200)||(y >= 100 && y <= 200)) true else false
+    return min(x, y) >= 100 && max(x, y) <= 200
 }
 fun onTwoOrThree(x: Int, y: Int, z: Int) :Boolean{
-    return if ((x >= 20 && x <= 50)||(y >= 20 && y <= 50)||(z >= 20 && z <= 50)) true else false
+//        return if ((x >= 20 && x <= 50)||(y >= 20 && y <= 50)||(z >= 20 && z <= 50)) true else false
+    return (x in 20..50 || y in 20 .. 50 || z in 20..50)
 }
 fun onOreTwo(x: Int, y: Int, z: Int) :Boolean{
-    var a = 0
-    if (x>=20 && x<= 50) a++
-    if (y>=20 && y<= 50) a++
-    if (z>=20 && z<= 50) a++
-    return if (a < 3 && a > 0) true else false
+//    var a = 0
+//    if (x in 20..50) a++
+//    if (y in 20..50) a++
+//    if (z in 20..50) a++
+//    return (a in 1 until 3)
+    return (x in 20..50 || y in 20..50 || z in 20..50)
 }
 fun nearestZero(x: Int, y: Int) :Int{
    return if (abs(100 - x) == abs(100 - y))  0 else if(abs(100 - x) > abs(100 - y)) return y else return x
 }
 fun onlyTwoIn(x: Int, y: Int) :Boolean{
-   return if((x >= 40 && x <=50 && y >= 40 && y <= 50) || (x >= 50 && x <=60 && y >= 50 && y <= 60)) true else false
+//   return if((x >= 40 && x <=50 && y >= 40 && y <= 50) || (x >= 50 && x <=60 && y >= 50 && y <= 60)) true else false
+    return (x in 40..50 && y in 40..50) || (x in 50..60 && y in 50..60)
 }
 fun oneValueIn(x: Int, y: Int) :Int{
-    return if((x >= 20 && x <= 30 && y >= 20 && y <= 30)) if (x >= y) x else y
-        else if (x >= 20 && x <= 30) x
-        else if (y >= 20 && y <= 30) y
-    else 0
+//    return if((x >= 20 && x <= 30 && y >= 20 && y <= 30)) if (x >= y) x else y
+//        else if (x >= 20 && x <= 30) x
+//        else if (y >= 20 && y <= 30) y
+//    else 0
+    return if(x >= y && x in 20..30 && y in 20..30) x else y
+        if (x in 20..30) x
+        else if (y in 20..30) y
+            else 0
 }
 fun nonNegativeValueSameLastDigit(x: Int, y: Int) :Boolean{
-    return if(x % 10 == y % 10) true else false
+    return x % 10 == y % 10
 }
 fun numbersequenceInArrayFinder(x: IntArray) :Boolean{
-    val arr123 = arrayOf(1, 2, 3)
-    val xlength= x.size-1
-    var i = 0
-    while(i<=xlength){
-        if (arr123.get(0) == x.get(i) && arr123.get(1) == x.get(i+1) && arr123.get(2) == x.get(i+2))
-        return true
-        i++
-    }
-    return false
+    val arr123 = intArrayOf(1,2,3)
+//    val xlength= x.size-1
+//    var i = 0
+//    while(i<=xlength){
+//        if (arr123.get(0) == x.get(i) && arr123.get(1) == x.get(i+1) && arr123.get(2) == x.get(i+2))
+//        return true
+//        i++
+//    }
+//    return false
+    for(i in 0 until x.size -1)
+        if (arr123[0] == x[i] && arr123[1] == x[i+1] && arr123[2] == x[i+2]) return true
+     return false
 }
 fun countNumber5NextTo(){
     val x = arrayOf(
         arrayOf(5, 5, 2, 3, 5, 5),
         arrayOf(2, 3, 5, 6, 7, 8, 9),
         arrayOf(5, 4, 3, 2, 1, 5, 5))
-    for (ar in x) {
-        var i = 0
-        var counn = 0
-        while (i < ar.size - 1) {
+    var counn = 0
+    for (ar in x)
+          for (i in 0 until ar.size - 1)
             if (ar.get(i) == 5 && (ar.get(i + 1) == 5 || ar.get(i + 1) == 6))
             counn++
-            i++
-        }
-        println(counn)
-    }
+    println(counn)
+
 }
 fun tripel(x: IntArray) :Boolean{
-    for (i in 0 until x.size - 2){
-
+    for (i in 0 until x.size - 2)
         if (x.get(i) == x.get(i + 1) && x.get(i) == x.get(i + 2))
         return true
-    }
     return false
 }
 fun sumInRange(x: Int, y: Int) :Int{
-    return if (x + y >= 10 && x + y <= 20) 30 else x + y
+//    return if (x + y >= 10 && x + y <= 20) 30 else x + y
+    return if (x + y in 10..20) 30 else x + y
+
 }
 fun fiveInOne(x: Int, y: Int) :Boolean{
-    return if (x == 5 || y == 5 || x + y == 5) true else false
+//    return if (x == 5 || y == 5 || x + y == 5) true else false
+    return x == 5 || y == 5 || x + y == 5
 }
-fun funny13(x: Int) : Boolean{
-    return if(x % 13 == 0 || x > 13) true else false
-}
+//fun funny13(x: Int) : Boolean{
+//    return if(x % 13 == 0 || x > 13) true else false
+//    return x % 13 == 0 || x > 13
+//}
+fun funny13(x: Int) = x % 13 == 0 || x > 13
 fun threeOreSeven(x: Int, y: Int) :Boolean{
-    return if(!(x % 3 == 0 && y % 7 == 0) && (x % 3 == 0 || y % 7 == 0)) true else false
+//    return if(!(x % 3 == 0 && y % 7 == 0) && (x % 3 == 0 || y % 7 == 0)) true else false
+    return !(x % 3 == 0 && y % 7 == 0) && (x % 3 == 0 || y % 7 == 0)
 }
 fun multi10under2(x: Int) :Boolean{
     return if(x % 10 <=2) true else false
